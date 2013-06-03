b0f3e4637bd9ea393e9f1f6bb8e8547bbb21f02f
Jasen Hall <jasen@cs.ucsb.edu>
2013-06-03 12:49:38 -0700

    Added comments to TreeNode.

--- TreeNode.py
+++ TreeNode.py
@@ -1,4 +1,11 @@
 class TreeNode:
+  """
+    Simple binary tree structure that supports insert and lookup. Does not
+    support delete and does not balance inserts in any way. To use, instance
+    a node, set key and value attributes. For additional nodes, repeat
+    steps for the root node to create a new child and call insert on the
+    root passing the new child.
+  """
 
   def __init__(self):
     self.lChild = 0
@@ -17,8 +24,8 @@ class TreeNode:
       return 0    # not found
 
   def insert(self, newNode):
-    if newNode.key == self.key:
-      return    # no duplicates, use findValueByKey() first
+    if newNode.key == self.key:   # no duplicates allowed
+      return    
     elif newNode.key > self.key:
       if self.rChild == 0:
         self.rChild = newNode
